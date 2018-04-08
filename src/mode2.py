03da84256637bad87ecad1cae61f6cd35adac8e5
Aboubakr OUDGHIRI <aboubakr.oudghiri@u-psud.fr>
2018-04-08 21:42:55 +0200

    mode 2 to be continued

--- src/mode2.py
+++ src/mode2.py
@@ -1,23 +1,56 @@
 import os
 import random
 import requests
+import re
 from random import randint
 
+
+
+def chatbot():
+    print("Welcome my name is Backchannels Bot ! I'm here to backchannel whatever you say !")
+    result = ("uh uh", 0)
+    while 1:
+        human = input("Me : ")
+        analyse(human)
+        
+
 def read_inputs(fileName):
-    file = open(fileName, "r")
     inputs = []
-    i = 0
-    j = 0
-
-    for line in file:
-        line_splited = line.split("\n")
-        inputs.append(line_splited[0])
-        i = i + 1
-
-
-    file.close()
+    lword = []
+    filepointer = open(fileName, "r")
+    for line in filepointer.readlines():
+        if line.strip() == "":
+            inputs.append(lword[:])
+            lword.clear()
+            continue
+        lword.append(line.strip("\n"))
+    filepointer.close()
     return inputs
 
+def analyse(sentence):
+    outputs = read_inputs("./../res/EN/outputs.txt")
+    inputs = read_inputs("./../res/EN/inputs.txt")
+    response = "Why "
+    request = ""
+    s = re.split(" ", sentence)
+    for i in range(len(inputs)):
+        if(s[0] == inputs[i][0]):
+            for j in range(len(inputs[i])):
+                tense = s[1]
+                if(s[1] == "will" and s[2] in ["be", "have"]):
+                    tense = s[1]+ " " +s[2] 
+                if(tense == inputs[i][j]):
+                    request = s[0]+" "+tense 
+                    response += outputs[i][j]
+                    break;
+
+    # ... la suite de la phrase
+    # print(request)
+    # print(inputs)
+    print(response + " " + sentence.strip(request) + "?")
+
+
+
 if __name__=="__main__":
-	inputs = read_inputs("./../res/inputs.txt")
-	print(inputs)
+    chatbot()
+	
