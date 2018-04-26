2b3d82eec524bf8f6a43a5ee6fa6004ff47c433f
douumaams <doumams.cleveland@gmail.com>
2018-04-26 17:43:51 +0200

    Mode 2 Advancements

--- src/mode2.py
+++ src/mode2.py
@@ -6,10 +6,92 @@ from random import randint
 
 def chatbot():
     print("Welcome my name is Backchannels Bot ! I'm here to backchannel whatever you say !")
-    result = ("uh uh", 0)
+    lans = []
     while 1:
         human = input("Me : ")
-        analyse(human)
+        analyse(human, lans)
+        if len(lans) > 2:
+            lans = lans[len(lans)-1:]
+        #print(lans)
+
+def analyse(sentence, lans):
+    ans = discussion(sentence, lans)
+    if ans == "":
+        ans = eliza(sentence)
+
+    lans.append(ans)
+
+    print("Bot: " + ans)
+
+
+def eliza(sentence):
+    outputs = read_inputs("./../res/EN/outputs.txt")
+    #print(outputs)
+    inputs = read_inputs("./../res/EN/inputs.txt")
+    #print(inputs)
+    response = "Why "
+    request = ""
+    s = re.split(" ", sentence)
+    #print(s)
+    for i in range(len(inputs)):
+        #print(inputs[i][0])
+        if(s[0] == inputs[i][0]):
+            for j in range(len(inputs[i])):
+                tense = s[1]
+                if(s[1] == "will" and s[2] in ["be", "have"]):
+                    tense = s[1]+ " " +s[2]
+                if(tense == inputs[i][j]):
+                    request = s[0]+" "+tense
+                    response += outputs[i][j]
+                    break;
+    if response == "Why ":
+        return "backchannel"
+    else:
+    # ... la suite de la phrase
+
+    #print("Bot: " + response + " " + sentence.strip(request) + "?")
+        return response + " " + sentence.strip(request) + "?"
+
+def discussion(sentence, lans):
+    answer = ""
+
+    splittedSentence = re.split(" ", sentence)
+    #print(splittedSentence)
+
+    for word in splittedSentence:
+        topic = findKeyword(word)
+
+    if topic != "":
+
+        answer = selectRandom(topic)
+        #print(answer)
+        if lans:
+            while answer == lans[-1]:
+                #print("bonjour")
+                answer = selectRandom(topic)
+
+    return answer
+
+def findKeyword(word):
+    vocs = read_voc()
+
+    for ltopic in vocs:
+        for keyword in ltopic:
+            if word == keyword:
+
+                return ltopic[0]
+    return ""
+
+def selectRandom(topic):
+    ans = read_ans()
+
+    for ltopic in ans:
+        if topic == ltopic[0]:
+            #print(topic)
+            n = randint(1,len(ltopic)-1)
+            return ltopic[n] #answer
+
+
 
 def read_inputs(fileName):
     inputs = []
@@ -22,6 +104,7 @@ def read_inputs(fileName):
             lword.clear()
             continue
         lword.append(line.strip("\n"))
+    inputs.append(lword[:])
     filepointer.close()
     #print(inputs)
     return inputs
@@ -75,7 +158,7 @@ def read_voc():
     lvoc.clear()
     filepointer.close()
 
-    print(ltopic)
+    #print(ltopic)
     return ltopic
 
 def read_ans():
@@ -101,7 +184,7 @@ def read_ans():
     filepointer.close()
 
     #cooking
-    filepointer = open("./../res/EN/cookingAns.txt","r")
+    filepointer = open("./../res/EN/cookAns.txt","r")
     for line in filepointer.readlines():
         lans.append(line.strip("\n"))
 
@@ -127,37 +210,10 @@ def read_ans():
     lans.clear()
     filepointer.close()
 
-    print(ltopic)
+    #print(ltopic)
     return ltopic
 
-def analyse(sentence):
-    outputs = read_inputs("./../res/EN/outputs.txt")
-    #print(outputs)
-    inputs = read_inputs("./../res/EN/inputs.txt")
-    #print(inputs)
-    vocs = read_voc()
-    ans = read_ans()
-    response = "Why "
-    request = ""
-    s = re.split(" ", sentence)
-    #print(s)
-    for i in range(len(inputs)):
-        #print(inputs[i][0])
-        if(s[0] == inputs[i][0]):
-            for j in range(len(inputs[i])):
-                tense = s[1]
-                if(s[1] == "will" and s[2] in ["be", "have"]):
-                    tense = s[1]+ " " +s[2]
-                if(tense == inputs[i][j]):
-                    request = s[0]+" "+tense
-                    response += outputs[i][j]
-                    break;
-
-    # ... la suite de la phrase
-
-    print("Bot: " + response + " " + sentence.strip(request) + "?")
-
 if __name__=="__main__":
-    #chatbot()
-    read_voc()
-    read_ans()
+    chatbot()
+    #read_voc()
+    #read_ans()
